f22e6a53ced20cdc992d6ad0d80835f8e873d58e
kaori-sato2025 <one.peace999sk@gmail.com>
2025-12-16 01:14:42 +0900

    database.py変更
    Updated database connection to use environment variables and SSL.

--- database.py
+++ database.py
@@ -1,16 +1,26 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
+# .env を読み込む
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# SSL接続を有効化
+ssl_args = {
+    "ssl": {
+        "ca": os.getenv("SSL_CA_PATH"),
+        "ssl_mode": os.getenv("DATABASE_SSL_MODE", "require")
+    }
+}
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=ssl_args)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
